bbea950db4ddfb06c60dba8df67c166ed5b2990a
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-11-20 07:35:50 -0800

    Adds 12 practice files for js and 12 for ruby with solutions links

---
+++ javascript/9_arrays_select_practice_problems.js
@@ -0,0 +1,35 @@
+// Select items from an array into a new array with items that match a certain condition
+
+//  1. Start with an array of numbers and create a new array with only the numbers less than 20.
+// For example, [2, 32, 80, 18, 12, 3] becomes [2, 18, 12, 3].
+
+//  2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
+// For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
+
+//  3. Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
+// For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
+
+//  4. Start with an array of numbers and create a new array with only the even numbers.
+// For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
+
+//  5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
+// For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
+
+//  6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
+// For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
+
+//  7. Start with an array of numbers and create a new array with only the numbers less than 10.
+// For example, [8, 23, 0, 44, 1980, 3] becomes [8, 0, 3].
+
+//  8. Start with an array of strings and create a new array with only the strings that don't start with the letter "b".
+// For example, ["big", "little", "good", "bad"] becomes ["little", "good"].
+
+//  9. Start with an array of hashes and create a new array with only the hashes with prices less than 10 (from the :price key).
+// For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "pencil", price: 1}, {name: "book", price: 4}].
+
+// 10. Start with an array of numbers and create a new array with only the odd numbers.
+// For example, [2, 4, 5, 1, 8, 9, 7] becomes [5, 1, 9, 7].
+
+// SOLUTIONS (using while loop): https://gist.github.com/peterxjang/d204a0c689d201ceefe2b8e639cc3a27
+// SOLUTIONS (using .each shortcut): https://gist.github.com/peterxjang/9dc4ae6d3029d95b6d7e88f056809bb9
+// SOLUTIONS (using .select shortcut): https://gist.github.com/peterxjang/88500dbd311a510940b6a693e65ec7f1
