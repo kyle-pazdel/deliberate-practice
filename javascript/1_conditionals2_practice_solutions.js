5e1f4a567808908416f57c2998720afb3839f743
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-11-20 08:30:43 -0800

    Deliberate practice 'Create an if statement that uses elsif and else with valid syntax from memory'in JavaScript (1-5)

--- javascript/1_conditionals2_practice_solutions.js
+++ javascript/1_conditionals2_practice_solutions.js
@@ -2,13 +2,63 @@
 
 //  1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
 
+// const v = 1;
+const v = 10;
+if (v === 10) {
+  console.log(0);
+} else {
+  console.log(-1);
+}
+
 //  2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+
+// const v = 9;
+// const v = 10;
+const v2 = 90;
+if (v2 < 10) {
+  console.log(-1);
+} else if (v > 10) {
+  console.log(1);
+} else {
+  console.log(0);
+}
 
 //  3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
 
+const v3 = 9;
+// const v3b = 1;
+const v3b = 10;
+if (v3 < 10 && v3b < 10) {
+  console.log(1);
+} else {
+  console.log(0);
+}
+
 //  4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
 
+// const v4 = 1000;
+const v4 = 10000;
+if (v4 > 9000) {
+  console.log(1);
+} else {
+  console.log(-1);
+}
+
 //  5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+
+// const v5 = 60;
+// const v5 = 22;
+// const v5 = 13;
+const v5 = 3;
+if (v5 < 10) {
+  console.log(9);
+} else if (v5 < 20) {
+  console.log(19);
+} else if (v5 < 30) {
+  console.log(29);
+} else {
+  console.log(-1);
+}
 
 //  6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
 
--- ruby/1_conditionals2_practice_problems.rb
+++ ruby/1_conditionals2_practice_problems.rb
@@ -1,4 +1,4 @@
-# Create an if statement that uses elsif and else with valid syntax from memory
+# 'Create an if statement that uses elsif and else with valid syntax from memory'
 
 # 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
 
