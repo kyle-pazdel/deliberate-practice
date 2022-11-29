c48a31882b92fd628c88a8ae11355902eae74ee2
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-11-29 15:21:04 -0800

    Deliberate practice 'Create a loop which modifies a variable each time the loop runs from memory' in JavaScript (1-5)

--- javascript/4_loops2_practice_problems.js
+++ javascript/4_loops2_practice_problems.js
@@ -1,14 +1,41 @@
 // Create a loop which modifies a variable each time the loop runs from memory
 
 // 1. Write a while loop to print the numbers 1 through 10.
+var i = 1;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
 
 // 2. Write a while loop that prints the word "hello" 5 times.
 
+var i2 = 0;
+while (i2 < 5) {
+  console.log("hello");
+  i2++;
+}
+
 // 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+var w = "";
+while (w.toLowerCase() !== "stop") {
+  w = window.prompt("Enter a word: ");
+}
 
 // 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
 
+var i3 = 0;
+while (i3 <= 100) {
+  console.log(i3);
+  i3 += 5;
+}
+
 // 5. Write a while loop that prints the number 9000 ten times.
+
+var i4 = 0;
+while (i4 < 10) {
+  console.log(9000);
+  i4++;
+}
 
 // 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
 
@@ -21,3 +48,5 @@
 // 10. Write a while loop that prints the even numbers from 2 to 40.
 
 // JavaScript solutions: https://gist.github.com/peterxjang/da9cdaf5fa764ecfbefedb2d339193aa
+
+// file:///Users/KylePazdel/deliberate-practice/JavaScript/test.html
