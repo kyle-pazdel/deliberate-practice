4c38bf791863392345206c26973a03c9e90da0ef
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-12-07 08:06:55 -0800

    Deliberate practice 'Use built-in methods for strings and numbers' in Ruby

--- ruby/3_methods_practice_problems.rb
+++ ruby/3_methods_practice_problems.rb
@@ -10,44 +10,41 @@ p word.upcase
 
 print "Enter a number: "
 num = gets.chomp
-
 if num.to_i > 100
   p "That's a big number"
 end
 
 # 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
 
-print "Enter a number: "
+print "Enter two numbers: "
 n1 = gets.chomp
-print "Enter a second number: "
 n2 = gets.chomp
 p n1.to_i + n2.to_i
 
 # 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
 
 print "Enter a word: "
-word = gets.chomp
-p word.reverse
+w = gets.chomp
+p w.reverse
 
 # 5. Write a program that asks the user to enter a number, then prints the number times 10.
 
 print "Enter a number: "
-num = gets.chomp
-p num.to_i * 10
+n = gets.chomp
+p n.to_i * 10
 
 # 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
 
-print "Enter a word: "
+print "Enter two words: "
 w1 = gets.chomp
-print "Enter another word: "
 w2 = gets.chomp
 p w1.upcase + " " + w2.upcase
 
 # 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
 
 print "Enter a word: "
-word = gets.chomp
-p word.length
+w = gets.chomp
+p w.length
 
 # 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
 
@@ -59,17 +56,16 @@ end
 
 # 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
 
-print "Enter a number: "
+print "Enter two numbers: "
 n1 = gets.chomp
-print "Enter another number: "
 n2 = gets.chomp
 p n1.to_i * n2.to_i
 
 # 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
 
 print "Enter a word: "
-word = gets.chomp
-if word.length > 5
+w = gets.chomp
+if w.length > 5
   p "That's a long word"
 end
 
