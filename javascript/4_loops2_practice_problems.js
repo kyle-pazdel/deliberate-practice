7b291af95633df5e1f3e4e802ec23cc71885d690
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-12-13 07:33:25 -0800

    Deliberate practice 'Create a loop which modifies a variable each time the loop runs from memory' in JavaScript

--- javascript/4_loops2_practice_problems.js
+++ javascript/4_loops2_practice_problems.js
@@ -1,46 +1,55 @@
 // Create a loop which modifies a variable each time the loop runs from memory
 
 // 1. Write a while loop to print the numbers 1 through 10.
-// var i = 1;
-// while (i <= 10) {
-//   console.log(i);
-//   i++;
-// }
+
+var i = 1;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
 
 // 2. Write a while loop that prints the word "hello" 5 times.
 
-// var i2 = 0;
-// while (i2 < 5) {
-//   console.log("hello");
-//   i2++;
-// }
+var i2 = 0;
+while (i2 < 5) {
+  console.log("hello");
+  i2++;
+}
 
 // 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
-// var w = "";
-// while (w.toLowerCase() !== "stop") {
-//   w = window.prompt("Enter a word: ");
-// }
+
+var go = true;
+while (go) {
+  const response = window.prompt("Enter a word: ");
+  if (response.toLowerCase() === "stop") {
+    go = false;
+  }
+}
 
 // 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
 
-// var i3 = 0;
-// while (i3 <= 100) {
-//   console.log(i3);
-//   i3 += 5;
-// }
+var i4 = 0;
+while (i4 <= 100) {
+  console.log(i4);
+  i4 += 5;
+}
 
 // 5. Write a while loop that prints the number 9000 ten times.
 
-// var i4 = 0;
-// while (i4 < 10) {
-//   console.log(9000);
-//   i4++;
-// }
+var i5 = 0;
+while (i5 < 10) {
+  console.log(9000);
+  i5++;
+}
 
 // 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
-var n6 = 0;
-while (parseInt(n6) <= 10) {
-  n6 = window.prompt("Enter a number");
+
+var go2 = true;
+while (go2) {
+  const response = window.prompt("Enter a number: ");
+  if (parseInt(response) > 10) {
+    go2 = false;
+  }
 }
 
 // 7. Write a while loop that prints the numbers 50 to 70.
@@ -53,17 +62,20 @@ while (i7 <= 70) {
 
 // 8. Write a while loop that prints the phrase "Around the world" 144 times.
 
-var i8 = 0;
-while (i8 < 144) {
+var i8 = 1;
+while (i8 <= 144) {
   console.log("Around the world");
   i8++;
 }
 
 // 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
 
-var w9 = "";
-while (w9.length <= 5) {
-  w9 = window.prompt("Enter a word");
+var go3 = true;
+while (go3) {
+  const response = window.prompt("Enter a word: ");
+  if (response.length > 5) {
+    go3 = false;
+  }
 }
 
 // 10. Write a while loop that prints the even numbers from 2 to 40.
