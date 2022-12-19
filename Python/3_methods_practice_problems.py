69dbaa48e6ea20b9a1308fc2c91b4e11e3ef3235
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-12-19 14:56:18 -0800

    Deliberate practice 'Use built-in methods for strings and numbers' in Python (1-10

--- Python/3_methods_practice_problems.py
+++ Python/3_methods_practice_problems.py
@@ -19,20 +19,45 @@ print(int(response) + int(response2))
 
 # 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
 
-# response = input("Enter a word: ")
-# print(response[::-1])
+response = input("Enter a word: ")
+print(response[::-1])
 
 
 # 5. Write a program that asks the user to enter a number, then prints the number times 10.
 
+response = input("Enter a number: ")
+i = 0
+while i < 10:
+  print(response)
+  i += 1
+
 # 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
+
+response = input("Enter a word: ")
+response2 = input("Enter another word: ")
+print(f"{response.upper()} {response2.upper()}")
 
 # 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
 
+response = input("Enter a word: ")
+print(len(response))
+
 # 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
+
+response = input("Enter a number: ")
+if int(response) < 0:
+  print("That's a negative number")
 
 # 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
 
+response = input("Enter a number: ")
+response2 = input("Enter another number: ")
+print(int(response) * int(response2))
+
 # 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
+
+response = input("Enter a word: ")
+if len(response) > 5:
+  print("That's a long word")
 
 # SOLUTIONS: tbd
