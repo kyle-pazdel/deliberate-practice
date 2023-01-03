8169da85383556c22130a02f62f71afa8e3c197a
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-01-03 15:46:16 -0800

    Deliberate practice 'Create a loop which modifies a variable each time the loop runs from memory' in Python with While Loops

--- Python/4_loops2_practice_problems.py
+++ Python/4_loops2_practice_problems.py
@@ -2,59 +2,70 @@
 
 # 1. Write a loop to print the numbers 1 through 10.
 
-for i in range(1, 11):
-  print(i)
-
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
 
 # 2. Write a loop that prints the word "hello" 5 times.
 
-for i in range(1, 6):
-  print("hello")
+i = 0
+while i < 5:
+    print("hello")
+    i += 1
 
 # 3. Write a loop that asks the user to enter a word and will run forever until the user enters the word "stop".
 
 response = ""
-while not response.lower() == "stop":
-  response = input("Enter a word: ")
+while response.lower() != "stop":
+    response = input("Enter a word: ")
 
 
 # 4. Write a loop that prints the numbers 0 through 100, increasing by 5 each time.
 
-for i in range(0, 101, 5):
-  print(i)
+i = 0
+while i <= 100:
+    print(i)
+    i += 5
 
 # 5. Write a loop that prints the number 9000 ten times.
 
-for i in range(1, 11):
-  print(9000)
-
+i = 1
+while i <= 10:
+    print(9000)
+    i += 1
 
 # 6. Write a loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
 
 response = 0
-while not int(response) > 10:
-  response = input("Enter a number: ")
-
+while int(response) <= 10:
+    response = input("Enter a number: ")
 
 # 7. Write a loop that prints the numbers 50 to 70.
 
-for i in range(50, 71):
-  print(i)
+i = 50
+while i <= 70:
+    print(i)
+    i += 1
 
 # 8. Write a loop that prints the phrase "Around the world" 144 times.
 
-for i in range(1, 145):
-  print(f"{i}: Around the world")
+i = 1
+while i <= 144:
+    print(f"{i} Around the world")
+    i += 1
 
 # 9. Write a loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
 
 response = ""
-while not len(response) > 5:
-  response = input("Enter a long word: ")
+while len(response) <= 5:
+    response = input("Enter a word: ")
 
 # 10. Write a loop that prints the even numbers from 2 to 40.
 
-for i in range(2, 41, 2):
-  print(i)
+i = 2
+while i <= 40:
+    print(i)
+    i += 2
 
 # SOLUTIONS While Loops: https://github.com/kyle-pazdel/deliberate-practice/blob/main/Python/solutions/4_loops2_practice_solutions.py.py
