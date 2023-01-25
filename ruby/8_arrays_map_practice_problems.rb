d7a9ac1cc9e6f3b8a76303e9f7e6643d1a8cde98
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-01-24 20:51:42 -0800

    Deliberate practice 'Map an array to a new array with some computation performed on each item' in Ruby with map method

--- ruby/8_arrays_map_practice_problems.rb
+++ ruby/8_arrays_map_practice_problems.rb
@@ -4,91 +4,91 @@
 #     For example, [1, 2, 3] becomes [3, 6, 9].
 
 arr = [1, 2, 3]
-new_arr = arr.map do |num|
-  num * 3
+new_arr = arr.map do |i|
+  i * 3
 end
 p new_arr
 
 #  2. Start with an array of strings and create a new array with each string upcased.
 #     For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"].
 
-array_of_strings = ["hello", "goodbye"]
-uppercase_strings = array_of_strings.map do |string|
-  string = string.upcase
+arr = ["hello", "goodbye"]
+new_arr = arr.map do |str|
+  str.upcase
 end
-p uppercase_strings
+p new_arr
 
 #  3. Start with an array of hashes and create a new array of string values from each hash's :name key.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
 
 people = [{ name: "Alice", age: 27 }, { name: "Blane", age: 16 }]
-names = people.map do |person|
-  name = person[:name]
+new_arr = people.map do |person|
+  person[:name]
 end
-p names
+p new_arr
 
 #  4. Start with an array of numbers and create a new array with each number plus 7.
 #     For example, [1, 2, 3] becomes [8, 9, 10].
 
 arr = [1, 2, 3]
 new_arr = arr.map do |num|
-  num = num + 7
+  num + 7
 end
 p new_arr
 
 #  5. Start with an array of strings and create a new array with each string's length.
 #     For example, ["hello", "goodbye"] becomes [5, 7].
 
-array_of_strings = ["hello", "goodbye"]
-strings_lengths = array_of_strings.map do |string|
-  length = string.length
+strings = ["hello", "goodbye"]
+new_arr = strings.map do |str|
+  str.length
 end
-p strings_lengths
+p new_arr
 
 #  6. Start with an array of hashes and create a new array of number values from each hash's :age key.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [27, 16].
 
 people = [{ name: "Alice", age: 27 }, { name: "Blane", age: 16 }]
-ages = people.map do |person|
-  age = person[:age]
+new_arr = people.map do |person|
+  person[:age]
 end
-p ages
+p new_arr
 
 #  7. Start with an array of numbers and create a new array with each number divided by 2.
 #     For example, [1, 2, 3] becomes [0.5, 1.0, 1.5].
 
-array_of_numbers = [1, 2, 3]
-new_array = array_of_numbers.map do |num|
-  calculated = num.to_f / 2
+arr = [1, 2, 3]
+new_arr = arr.map do |num|
+  num / 2.0
 end
-p new_array
+p new_arr
 
 #  8. Start with an array of strings and create a new array with each string's first letter only.
 #     For example, ["hello", "goodbye"] becomes ["h", "g"].
 
-array_of_strings = ["hello", "goodbye"]
-first_letters = array_of_strings.map do |string|
-  first_letter = string[0]
+strings = ["hello", "goodbye"]
+new_arr = strings.map do |str|
+  str[0]
 end
-p first_letters
+p new_arr
 
 # 9.  Start with an array of hashes and create a new array of number values from each hash's :age key times 2.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [54, 32].
 
 people = [{ name: "Alice", age: 27 }, { name: "Blane", age: 16 }]
-ages_doubled = people.map do |person|
-  age_doubled = person[:age] * 2
+new_arr = people.map do |person|
+  person[:age] * 2
 end
-p ages_doubled
+p new_arr
 
 # 10. Start with an array of numbers and create a new array with each number converted into a string.
 #     For example, [1, 2, 3] becomes ["1", "2", "3"].
 
-array_of_numbers = [1, 2, 3]
-new_array = array_of_numbers.map do |num|
-  num_to_string = num.to_s
+arr = [1, 2, 3]
+new_arr = arr.map do |num|
+  num.to_s
 end
-p new_array
+p new_arr
 
 # SOLUTIONS (using while loop): https://gist.github.com/peterxjang/b9ac4390aad2301a2238efc95c904f3d
 # SOLUTIONS (using .each shortcut): https://gist.github.com/peterxjang/66598fd7cdbc67fe663624e217cebbaf
